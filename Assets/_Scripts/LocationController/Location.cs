d43bc0103c5b000e1b6d7b30a992ab636f3e42d2
DatDz <Keepyoursmile.512@gmail.com>
2019-01-16 14:59:43 +0700

    no message

--- Assets/_Scripts/LocationController/Location.cs
+++ Assets/_Scripts/LocationController/Location.cs
@@ -1103,7 +1103,7 @@ public class Location : MonoBehaviour
                 IDLocation = 0;
             }
 
-            if (id == 0 && (countType + 1 == 2 || countType + 1 == 3))
+            if (id == 0 && (countType + 1 == 1 || countType + 1 == 2))
             {
                 UIManager.Instance.panelWarning.SetActive(true);
                 UIManager.Instance.btnYesWarning.onClick.RemoveAllListeners();
