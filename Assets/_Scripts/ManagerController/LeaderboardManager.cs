84153eda1dd3517c2bb02797fe69920138144a1d
DatDz <Keepyoursmile.512@gmail.com>
2019-01-17 11:58:22 +0700

    fix LD

---
+++ Assets/_Scripts/ManagerController/LeaderboardManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms.GameCenter;
+
+/// <summary>
+/// Leaderboard manager.
+/// Auther : Nikunj Rola
+/// </summary>
+
+public class LeaderboardManager : MonoBehaviour 
+{
+	#region GAME_CENTER	
+
+	/// <summary>
+	/// Authenticates to game center.
+	/// </summary>
+    bool isLoginSuccessful;
+    string leaderBoardID = "PROVIDE_YOUR_LEADERBOARD_ID_HERE";
+	public void  AuthenticateToGameCenter()
+	{
+		#if UNITY_IPHONE
+		Social.localUser.Authenticate(success =>
+		                              {
+			if (success)
+			{
+                isLoginSuccessful = true;
+				Debug.Log("Authentication successful");
+			}
+			else
+			{
+                isLoginSuccessful = false;
+				Debug.Log("Authentication failed");
+			}
+		});
+		#endif
+	}
+
+	/// <summary>
+	/// Reports the score on leaderboard.
+	/// </summary>
+	/// <param name="score">Score.</param>
+	/// <param name="leaderboardID">Leaderboard I.</param>
+
+	public void ReportScore(long score, string leaderboardID)
+	{
+		#if UNITY_IPHONE
+		//Debug.Log("Reporting score " + score + " on leaderboard " + leaderboardID);
+        if (isLoginSuccessful)
+        {
+            Social.ReportScore(score, leaderboardID, success =>
+               {
+                   if (success)
+                   {
+                       Debug.Log("Reported score successfully");
+                   }
+                   else
+                   {
+                       Debug.Log("Failed to report score");
+                   }
+
+                   Debug.Log(success ? "Reported score successfully" : "Failed to report score"); Debug.Log("New Score:" + score);
+               });
+        }
+        else
+        {
+            AuthenticateToGameCenter();
+        }
+		#endif
+	}
+
+	/// <summary>
+	/// Shows the leaderboard UI.
+	/// </summary>
+
+    public void ShowLeaderboard(string leaderboardID)
+	{
+		#if UNITY_IPHONE
+			//Social.ShowLeaderboardUI();
+        GameCenterPlatform.ShowLeaderboardUI(leaderboardID, UnityEngine.SocialPlatforms.TimeScope.AllTime);
+		#endif
+	}
+	#endregion
+}
